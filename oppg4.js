a32974ccad55b5c94ecb54f893b888ba6a7e457e
Kristianmyhre <95939731+Kristianmyhre@users.noreply.github.com>
2021-12-10 21:21:15 +0100

    Add files via upload

---
+++ oppg4.js
@@ -0,0 +1,11 @@
+let fotballklubber = [
+    "barcelona",
+  "real madrid",
+  "manchester united"
+] 
+
+fotballklubber.push('bayern')
+fotballklubber.sort()
+console.log(fotballklubber.shift())
+console.log(fotballklubber)
+
